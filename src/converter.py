7ef7cc2d972c8e572ea878acd4fb2f4fc898a643
kadukitesesi <kadukitesesi@gmail.com>
2025-03-05 17:55:52 -0300

    segundo

--- src/converter.py
+++ src/converter.py
@@ -3,9 +3,17 @@ import os
 
 
 def converter_txt_pdf(caminho,saida):
-    with open(caminho,"r",encoding="utf-8") as file:
+
+    arquivo_txt = f"c:/Users/kaduk/OneDrive/Documentos/conversor-arquivos/input/{caminho}.txt"
+
+    with open(arquivo_txt, "w", encoding="utf-8") as file:
+        file.write("Este é o conteúdo que estou escrevendo no arquivo.\n")
+        file.write("Adicionar mais conteúdo aqui...\n")
+
+    with open(arquivo_txt, "r", encoding="utf-8") as file:
         linhas = file.readlines()
         print(linhas)
+
 
     pdf = canvas.Canvas(f"c:/Users/kaduk/OneDrive/Documentos/conversor-arquivos/output/{saida}.pdf")
     pdf.setFont("Helvetica",12)
@@ -24,4 +32,4 @@ def converter_txt_pdf(caminho,saida):
     pdf.save()        
 
 
-converter_txt_pdf("c:/Users/kaduk/OneDrive/Documentos/conversor-arquivos/input/input.txt")
+converter_txt_pdf("souls","like")
